737c87daca666cd36437813ae010b738ae4570bd
Speiger <speiger@gmx.net>
2020-11-26 22:56:15 +0100

    More work on ArrayLists. Adding new functions.

--- src/main/java/speiger/src/collections/utils/SanityChecks.java
+++ src/main/java/speiger/src/collections/utils/SanityChecks.java
@@ -2,6 +2,8 @@ package speiger.src.collections.utils;
 
 public class SanityChecks
 {
+	public static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+	
 	public static byte castToByte(int value)
 	{
 		if(value > Byte.MAX_VALUE || value < Byte.MIN_VALUE) throw new IllegalStateException("Value ["+value+"] out of Byte[-128,127] range");
@@ -26,5 +28,12 @@ public class SanityChecks
 		if(Double.isInfinite(value)) return value > 0 ? Float.POSITIVE_INFINITY : Float.NEGATIVE_INFINITY;
 		if(value < -Float.MAX_VALUE || value > Float.MAX_VALUE) throw new IllegalStateException("Value ["+value+"] out of Float range");
 		return (float)value;
+	}
+	
+	public static void checkArrayCapacity(int arraySize, int offset, int accessSize)
+	{
+		if(offset < 0) throw new IllegalStateException("Offset is negative ("+offset+")");
+		else if(accessSize < 0) throw new IllegalStateException("Size is negative ("+accessSize+")");
+		else if(arraySize < offset + accessSize) throw new IndexOutOfBoundsException("Index (" + offset + accessSize + ") is not in size (" + arraySize + ")");
 	}
 }
