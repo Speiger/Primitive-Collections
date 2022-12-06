d44ad2d42e0119dabb895c264f0a11fd6b2e4f6c
Speiger <speiger@gmx.net>
2022-12-06 04:57:43 +0100

    99% of the Set Module done.
    The 1% is the Distinct Iterator that relies on a Set implementation.
    So I have to make a dedicated set implementation.

--- src/builder/java/speiger/src/builder/PrimitiveCollectionsBuilder.java
+++ src/builder/java/speiger/src/builder/PrimitiveCollectionsBuilder.java
@@ -239,8 +239,8 @@ public class PrimitiveCollectionsBuilder extends TemplateProcessor
 			boolean force = flags.contains("force");
 			boolean tests = flags.contains("tests");
 			boolean forceTests =  flags.contains("force-tests");
-			boolean load = flags.contains("load");
-			boolean save = !flags.contains("save");
+			boolean load = !flags.contains("load");
+			boolean save = flags.contains("save");
 			int flag = (load ? LOAD : 0) | (save ? SAVE : 0);
             new PrimitiveCollectionsBuilder(silent).setFlags(flag).process(force);
             if(tests) {
